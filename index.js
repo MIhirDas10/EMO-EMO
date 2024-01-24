a2167d1fa1413e1bd3901b11363dae92f8cfc64e
MIhirDas10 <69119099+MIhirDas10@users.noreply.github.com>
2024-01-24 09:18:18 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const myBox=document.getElementById('myBox');
+const myButton=document.getElementById('myButton');
+myButton.addEventListener('click',event=>{
+    myBox.style.backgroundColor='tomato';
+    myBox.textContent='OUCH! 🤕';
+});
+myButton.addEventListener('mouseover',event=>{
+    myBox.style.backgroundColor='yellow';
+    myBox.textContent="Don't do it 😠";
+});
+myButton.addEventListener('mouseout',event=>{
+    myBox.style.backgroundColor='lightgreen';
+    myBox.textContent='Click Me 😊';
+});
